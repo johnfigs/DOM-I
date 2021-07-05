e551311e86f203abe405c4119118cfa8bc3519e6
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-07-05 16:21:30 -0700

    Completed adding Images

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const codeSnipImg = document.querySelector('#cta-img');
+codeSnipImg.setAttribute('src', 'img/header-img.png');
+
+const middleImg = document.querySelector('#middle-img');
+middleImg.setAttribute('src', '/img/mid-page-accent.jpg');
